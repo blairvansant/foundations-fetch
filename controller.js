e12524f3aa1157d24d45775d5727211653b34d32
blairvansant <blairbot@gmail.com>
2017-01-12 12:49:11 -0600

    add functionality to dropdown

--- controller.js
+++ controller.js
@@ -2,19 +2,56 @@
   angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
     $scope.responses = [];
     $scope.url = "http://httpstat.us/200";
-      $scope.fetch = () => {
-          const initialDate = (new Date()).getTime();
-          requestService.get($scope.url).then((success) => {
+
+    $scope.fetch = () => {
+      const initialDate = (new Date()).getTime();
+      if ($scope.method == 'GET') {
+        console.log('method calls', $scope.method)
+        requestService.get($scope.url).then((success) => {
           const receivedDate = (new Date()).getTime();
           const response = new Response(success);
           const responseTime = receivedDate - initialDate;
           response.responseTime = responseTime;
           console.log("responseTime", responseTime);
           $scope.responses.push(response);
-         }, (error) => {
-        // debugger
-        //do something else
-        });
-      };
-    });
+          console.log($scope.method)
+        }, (error) => {
+                // debugger
+              })
+      } else if ($scope.method == 'POST'){
+        requestService.post($scope.url).then((success) => {
+          const receivedDate = (new Date()).getTime();
+          const response = new Response(success);
+          const responseTime = receivedDate - initialDate;
+          response.responseTime = responseTime;
+          // const response = new Response(success);
+          $scope.responses.push(response);
+          console.log($scope.method)
+        }, (error) => {
+                // debugger
+              })
+      } else if ($scope.method == 'OPTIONS') {
+        requestService.options($scope.url).then((success) => {
+           const response = new Response(success);
+           $scope.responses.push(response);
+            console.log($scope.method)
+        }, (error) => {
+                //     debugger
+              })
+      } else if ($scope.method = 'HEAD') {
+        requestService.head($scope.url).then((success) => {
+            const response = new Response(success);
+            const receivedDate = (new Date()).getTime();
+            // const response = new Response(success);
+            const responseTime = receivedDate - initialDate;
+            response.responseTime = responseTime;
+            $scope.responses.push(response);
+            console.log($scope.method)
+          }, (error) => {
+                // debugger
+              })
+           } else {
+          }
+        };
+      });
 })();
